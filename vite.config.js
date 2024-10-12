f272628ba1ef37fb4aab2d2669810ed430a7d341
Yuliana Hapsari <yuliahapz@gmail.com>
2024-10-12 14:36:46 +0700

    solving bug deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback : true,
   }
 })
